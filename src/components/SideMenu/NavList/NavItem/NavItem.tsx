ebfe67f2cd358c40ae68d8c9757a2d8dbe614b85
scoutb6b <scoutboq@gmail.com>
2024-04-25 08:50:24 +0900

    ✨ SideMenuの実装

---
+++ src/components/SideMenu/NavList/NavItem/NavItem.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+interface NavItemProps {
+  label: string;
+  link: string;
+  icon: React.ReactNode;
+}
+
+const NavItem: React.FC<NavItemProps> = ({ label, link, icon }) => {
+  const pathname = usePathname();
+  return (
+    <Link
+      href={link}
+      className={`flex p-4 items-center w-full hover:bg-gray-300 font-medium
+      ${pathname === link ? "bg-gray-500 border-r-4 border-r-orange-300" : ""}`}
+    >
+      <div>{icon}</div>
+      <div className="pl-2">{label}</div>
+    </Link>
+  );
+};
+
+export default NavItem;
