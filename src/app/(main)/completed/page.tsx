3120933690b3db5142924932be41cccface24d4a
scoutb6b <scoutboq@gmail.com>
2024-05-28 06:27:23 +0900

    ✨ 取得したtaskをそれぞれのページに表示

---
+++ src/app/(main)/completed/page.tsx
@@ -0,0 +1,31 @@
+import TaskCard from "@/components/TaskCard/TaskCard";
+import { TaskDocument } from "@/models/task";
+
+const getCompletedTasks = async (): Promise<TaskDocument[]> => {
+  const responce = await fetch(`${process.env.API_URL}/tasks/completed`, {
+    cache: "no-store",
+  });
+  if (responce.status !== 200) {
+    throw new Error();
+  }
+  const data = await responce.json();
+  return data.tasks as TaskDocument[];
+};
+
+const ComletedTaskPage = async () => {
+  const completedTasks = await getCompletedTasks();
+  return (
+    <div className="text-gray-800 p-8 h-full overflow-y-auto pb-24">
+      <header className="flex justify-between items-center">
+        <h1 className="text-2xl font-bold flex items-center">All Tasks</h1>
+      </header>
+      <div className="mt-8 felx flex-wrap gap-4">
+        {completedTasks.map((task) => (
+          <TaskCard key={task._id} task={task} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ComletedTaskPage;
--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,5 +1,41 @@
-const Home = () => {
-  return <div>hi nextjs</div>;
+import TaskCard from "@/components/TaskCard/TaskCard";
+import { TaskDocument } from "@/models/task";
+import Link from "next/link";
+import { MdAddTask } from "react-icons/md";
+
+const getAllTasks = async (): Promise<TaskDocument[]> => {
+  const responce = await fetch(`${process.env.API_URL}/tasks`, {
+    cache: "no-store",
+  });
+  if (responce.status !== 200) {
+    throw new Error();
+  }
+  const data = await responce.json();
+  return data.tasks as TaskDocument[];
 };
 
-export default Home;
+const MainPage = async () => {
+  const allTasks = await getAllTasks();
+
+  return (
+    <div className="text-gray-800 p-8 h-full overflow-y-auto pb-24">
+      <header className="flex justify-between items-center">
+        <h1 className="text-2xl font-bold flex items-center">All Tasks</h1>
+        <Link
+          href="/new"
+          className="flex items-center gap-1 font-semibold border px-4 py-2 rounded-full shadow-sm text-white bg-gray-800 hover:bg-gray-700"
+        >
+          <MdAddTask className="size-5" />
+          <div className="pl-1">新規追加</div>
+        </Link>
+      </header>
+      <div className="mt-8 felx flex-wrap gap-4">
+        {allTasks.map((task) => (
+          <TaskCard key={task.id} task={task} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default MainPage;
