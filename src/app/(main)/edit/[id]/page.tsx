dfb2ebe9a227101c7cd01e73acba3f24d71939b0
scoutb6b <scoutboq@gmail.com>
2024-05-28 06:28:07 +0900

    ✨ それぞれのeditページを作成

---
+++ src/app/(main)/edit/[id]/page.tsx
@@ -0,0 +1,26 @@
+import EditTaskForm from "@/components/EditTaskForm/EditTaskForm";
+import { TaskDocument } from "@/models/task";
+
+interface Params {
+  params: { id: string };
+}
+const getTask = async (id: string): Promise<TaskDocument> => {
+  const responce = await fetch(`${process.env.API_URL}/tasks/${id}`, {
+    cache: "no-store",
+  });
+  const data = await responce.json();
+  return data.task as TaskDocument;
+};
+const EditTaskPage = async ({ params }: Params) => {
+  const id = params.id;
+  const task = await getTask(id);
+
+  return (
+    <div className="flex flex-col justify-center py-20 ">
+      <h2 className="text-center text-2xl font-bold">編集</h2>
+      <EditTaskForm task={task} />
+    </div>
+  );
+};
+
+export default EditTaskPage;
